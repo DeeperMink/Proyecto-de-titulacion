1d44e7ce2394714d603449c1080b303fda422f76
LuisSas <luis@makingdevs.com>
2018-11-27 23:12:26 -0600

    #23 Se agrega gráfica con datos en vue, al para tomar diferentes años de la base

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,53 @@
+Vue.use(VueHighcharts);
+
+var options = {
+  title: {
+    text: 'Gráfica de ganancias',
+    x: -20 //center
+  },
+  subtitle: {
+    text: 'BiReport',
+    x: -20
+  },
+  xAxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ]
+  },
+  yAxis: {
+    title: {
+      text: 'Temperature (°C)'
+    },
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'
+    }]
+  },
+  tooltip: {
+    valueSuffix: '°C'
+  },
+  legend: {
+    layout: 'vertical',
+    align: 'right',
+    verticalAlign: 'middle',
+    borderWidth: 0
+  },
+  series: [{
+    name: 'Tokyo',
+    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+  }, {
+    name: 'New York',
+    data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+  }, {
+    name: 'Berlin',
+    data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+  }, {
+    name: 'London',
+    data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+  }]
+};
+
 var data = {
   username: "",
   clients:{
@@ -45,6 +95,52 @@ var data = {
   },
   exampleBar:{
     width: "78%"
+  },
+
+  options:{
+    title: {
+      text: 'Gráfica de ganancias',
+      x: -20 //center
+    },
+    subtitle: {
+      text: 'BiReport',
+      x: -20
+    },
+    xAxis: {
+      categories: []
+    },
+    yAxis: {
+      title: {
+        text: 'Temperature (°C)'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      valueSuffix: '°C'
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    },
+    series: [{
+      name: 'Tokyo',
+      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2]
+    }, {
+      name: 'New York',
+      data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8]
+    }, {
+      name: 'Berlin',
+      data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6]
+    }, {
+      name: 'London',
+      data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0]
+    }]
   }
 }
 
@@ -151,6 +247,7 @@ const app = new Vue({
           }
           this.sales = salesList;
           this.totalAmountOfSales();
+          this.getAllYearsForChart();
         })
     },
 
@@ -162,6 +259,14 @@ const app = new Vue({
       console.log("Ventas totales:", total)
       this.totalSales = total;
     },
+
+    getAllYearsForChart: function(){
+      let years = [];
+      this.sales.forEach(function(element){
+        years.push(element.year);
+      })
+      this.options.xAxis.categories = years.filter((v, i, a) => a.indexOf(v) === i).sort()
+    }
   },
 
   beforeMount() {
